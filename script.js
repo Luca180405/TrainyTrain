d370c1e2f69593ffb81e6670b78c505ad2489492
Luca <65894806+Luca180405@users.noreply.github.com>
2021-08-10 22:12:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function fn1() {
     var str = document.getElementById("text1").value;
 }
 
+
+var circle = document.querySelectorAll('.train');
+
+train.addEventListener('animationend', () => {
+    location.href = 'index.html';
+});
